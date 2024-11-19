d41754e485f429976ce3a476571a18a2def61e9f
Albert Zhao <albertz@bu.edu>
2024-11-18 19:27:19 -0500

    adding new css implemented interface for the front end

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // We can add this for any global styles
 import App from './App';
 
-const theme = createTheme();
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
