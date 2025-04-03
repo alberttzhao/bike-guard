1c38c13b1dd7d49a9705101e4d135f40e7b5bfb9
ruizmar1 <MarissaRuiz2003@gmail.com>
2025-04-03 16:09:03 -0400

    Update picam3.py

--- hardware/picam3.py
+++ hardware/picam3.py
@@ -34,5 +34,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=5001, debug=False, threaded=True)
 
