32ff6494e46d3178fb2075a6219f197507116837
ruizmar1 <MarissaRuiz2003@gmail.com>
2025-04-07 19:50:46 -0400

    Update boot_try.py

--- hardware/boot_try.py
+++ hardware/boot_try.py
@@ -1,44 +1,43 @@
-#!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
 import subprocess
-import os
-import signal
 
 GPIO.setmode(GPIO.BCM)
 SWITCH_GPIO = 27
 GPIO.setup(SWITCH_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-process = None  # Will hold the accelerometer.py process
+process = None
 
 def start_script():
     global process
     if process is None:
-        print("Switch ON -- starting accelerometer.py...")
-        process = subprocess.Popen(["python3", "/home/pi/accelerometer.py"])
+        print("Switch ON – starting accelerometer.py...")
+        process = subprocess.Popen(["python3", "/home/Team8-c/bike-guard/hardware/accelerometer.py"])
+    else:
+        print("Script is already running.")
 
 def stop_script():
     global process
     if process:
-        print("Switch OFF -- stopping accelerometer.py...")
-        process.terminate()
+        print("Switch OFF – stopping accelerometer.py...")
+        process.terminate()  # Gracefully terminate the process
         try:
-            process.wait(timeout=5)
+            process.wait(timeout=5)  # Wait for the process to terminate
         except subprocess.TimeoutExpired:
-            process.kill()
+            print("Process did not stop in time, forcing termination...")
+            process.kill()  # Kill the process if it's still running after timeout
         process = None
+    else:
+        print("No process to stop.")
 
 try:
     last_state = GPIO.input(SWITCH_GPIO)
-    if last_state == GPIO.LOW:
-        start_script()
-
     while True:
         current_state = GPIO.input(SWITCH_GPIO)
         if current_state != last_state:
-            if current_state == GPIO.LOW:
+            if current_state == GPIO.LOW:  # Switch is ON
                 start_script()
-            else:
+            else:  # Switch is OFF
                 stop_script()
             last_state = current_state
         time.sleep(0.1)
