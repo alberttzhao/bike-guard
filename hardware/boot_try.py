898cff514eb5b0096a0036f91cbaba4722677c17
marghepiana <mpiana@bu.edu>
2025-04-30 16:36:42 -0400

    marissa trying

--- hardware/boot_try.py
+++ hardware/boot_try.py
@@ -1,14 +1,66 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+# import time
+# import subprocess
+
+
+
+# GPIO.setmode(GPIO.BCM)
+# SWITCH_GPIO = 27
+# GPIO.setup(SWITCH_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# buzzer_pin = 17 
+# GPIO.setup(buzzer_pin, GPIO.OUT)
+
+# process = None
+
+# def start_script():
+#     global process
+#     if process is None:
+#         print("Switch ON - starting accelerometer.py...")
+#         process = subprocess.Popen(["sudo","python3", "/home/Prova/bike-guard/hardware/accelerometer.py"])
+#     else:
+#         print("Script is already running.")
+
+# def stop_script():
+#     global process
+#     if process:
+#         print("Switch OFF - stopping accelerometer.py...")
+#         process.terminate()  # Gracefully terminate the process
+#         try:
+#             process.wait(timeout=5)  # Wait for the process to terminate
+#         except subprocess.TimeoutExpired:
+#             print("Process did not stop in time, forcing termination...")
+#             process.kill()  # Kill the process if it's still running after timeout
+#         process = None
+#     else:
+#         print("No process to stop.")
+
+# try:
+#     last_state = GPIO.input(SWITCH_GPIO)
+#     while True:
+#         current_state = GPIO.input(SWITCH_GPIO)
+#         if current_state != last_state:
+#             if current_state == GPIO.LOW:  # Switch is ON
+#                 start_script()
+#             else:  # Switch is OFF
+#                 stop_script()
+#                 GPIO.output(buzzer_pin, GPIO.LOW) # Turn buzzer OFF
+#             last_state = current_state
+#         time.sleep(0.1)
+# except KeyboardInterrupt:
+#     stop_script()
+# finally:
+#     GPIO.cleanup()
+
+import lgpio
 import time
 import subprocess
 
-
-
-GPIO.setmode(GPIO.BCM)
 SWITCH_GPIO = 27
-GPIO.setup(SWITCH_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-buzzer_pin = 17 
-GPIO.setup(buzzer_pin, GPIO.OUT)
+BUZZER_GPIO = 17
+
+h = lgpio.gpiochip_open(0)
+lgpio.gpio_claim_input(h, SWITCH_GPIO)
+lgpio.gpio_claim_output(h, BUZZER_GPIO, 0)
 
 process = None
 
@@ -16,7 +68,7 @@ def start_script():
     global process
     if process is None:
         print("Switch ON - starting accelerometer.py...")
-        process = subprocess.Popen(["sudo","python3", "/home/Prova/bike-guard/hardware/accelerometer.py"])
+        process = subprocess.Popen(["sudo", "python3", "/home/Prova/bike-guard/hardware/accelerometer.py"])
     else:
         print("Script is already running.")
 
@@ -24,29 +76,28 @@ def stop_script():
     global process
     if process:
         print("Switch OFF - stopping accelerometer.py...")
-        process.terminate()  # Gracefully terminate the process
+        process.terminate()
         try:
-            process.wait(timeout=5)  # Wait for the process to terminate
+            process.wait(timeout=5)
         except subprocess.TimeoutExpired:
-            print("Process did not stop in time, forcing termination...")
-            process.kill()  # Kill the process if it's still running after timeout
+            print("Forcing termination...")
+            process.kill()
         process = None
-    else:
-        print("No process to stop.")
+    # Turn off buzzer
+    lgpio.gpio_write(h, BUZZER_GPIO, 0)
 
 try:
-    last_state = GPIO.input(SWITCH_GPIO)
+    last_state = lgpio.gpio_read(h, SWITCH_GPIO)
     while True:
-        current_state = GPIO.input(SWITCH_GPIO)
+        current_state = lgpio.gpio_read(h, SWITCH_GPIO)
         if current_state != last_state:
-            if current_state == GPIO.LOW:  # Switch is ON
+            if current_state == 0:
                 start_script()
-            else:  # Switch is OFF
+            else:
                 stop_script()
-                GPIO.output(buzzer_pin, GPIO.LOW) # Turn buzzer OFF
             last_state = current_state
         time.sleep(0.1)
 except KeyboardInterrupt:
     stop_script()
 finally:
-    GPIO.cleanup()
+    lgpio.gpiochip_close(h)
